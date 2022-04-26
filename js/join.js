259042fcc754a4ead2c7e4dd2590f625c4675e8d
chahyunwoo <chahyunwoobi@gmail.com>
2022-04-26 11:26:28 +0900

    TODO: GALLERY

--- js/join.js
+++ js/join.js
@@ -138,3 +138,145 @@ function isInterest(name) {
     return false;
   }
 }
+
+const formMo = document.querySelector(".joinFormMo");
+const btnSubmitMo = formMo.querySelector("input[type=submit]");
+
+btnSubmitMo.addEventListener("click", (e) => {
+  if (!isIdMo("userIdMo", 6)) {
+    e.preventDefault();
+  }
+
+  if (!isPasswordMo("pwd1Mo", "pwd2Mo", 8)) {
+    e.preventDefault();
+  }
+
+  if (!isEmailMo("emailMo")) {
+    e.preventDefault();
+  }
+
+  if (!isCheckMo("genderMo")) {
+    e.preventDefault();
+  }
+
+  if (!isInterestMo("interestMo")) {
+    e.preventDefault();
+  }
+});
+
+function isIdMo(name, len = 6) {
+  let input = formMo.querySelector(`[name=${name}]`);
+  let txt = input.value;
+
+  const userIdBox = formMo.querySelector("#userIdMo");
+
+  if (txt.length >= len) {
+    userIdBox.classList.remove("error");
+
+    return true;
+  } else {
+    userIdBox.classList.add("error");
+
+    return false;
+  }
+}
+
+function isPasswordMo(name1, name2, len = 8) {
+  let pwd = formMo.querySelector(`[name=${name1}]`);
+  let rePwd = formMo.querySelector(`[name=${name2}]`);
+
+  let pwdValue = pwd.value;
+  let rePwdValue = rePwd.value;
+
+  const num = /[0-9]/;
+  const eng = /[a-zA-Z]/;
+  const spc = /[~!@#$%^&*()_+?><]/;
+
+  const rePwdBox = formMo.querySelector("#pwd2Mo");
+  const pwdBox = formMo.querySelector("#pwd1Mo");
+
+  if (
+    num.test(pwdValue) &&
+    eng.test(pwdValue) &&
+    spc.test(pwdValue) &&
+    pwdValue.length >= len
+  ) {
+    if (pwdValue === rePwdValue) {
+      rePwdBox.classList.remove("error");
+      pwdBox.classList.remove("error");
+      return true;
+    } else {
+      pwdBox.classList.remove("error");
+      rePwdBox.classList.add("error");
+    }
+  } else {
+    pwdBox.classList.add("error");
+    return false;
+  }
+}
+
+function isEmailMo(name) {
+  let input = formMo.querySelector(`[name=${name}]`);
+  let txt = input.value;
+  let regEmail =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  const userEmailBox = formMo.querySelector("#emailMo");
+
+  if (regEmail.test(txt)) {
+    userEmailBox.classList.remove("error");
+
+    return true;
+  } else {
+    userEmailBox.classList.add("error");
+
+    return false;
+  }
+}
+
+function isCheckMo(name) {
+  let inputs = formMo.querySelectorAll(`[name=${name}]`);
+  let isCheck = false;
+
+  const userGenderH2 = formMo.querySelector(".genderTitle");
+
+  for (let input of inputs) {
+    if (input.checked) {
+      isCheck = true;
+    }
+  }
+
+  if (isCheck) {
+    userGenderH2.classList.remove("error");
+
+    return true;
+  } else {
+    userGenderH2.classList.add("error");
+
+    return false;
+  }
+}
+
+function isInterestMo(name) {
+  let inputs = formMo.querySelectorAll(`[name=${name}]`);
+  let isCheck = false;
+
+  const userInterestH2 = formMo.querySelector(".interestTitle");
+
+  for (let input of inputs) {
+    if (input.checked) {
+      isCheck = true;
+    }
+  }
+
+  if (isCheck) {
+    userInterestH2.classList.remove("error");
+
+    return true;
+  } else {
+    userInterestH2.classList.add("error");
+
+    return false;
+  }
+}
+
